f373691e9d361fbfb6e2aa4f0de6f2f2a7cc1c18
Ivan Sharapov <oldrum75@gmail.com>
2022-03-23 23:02:43 +0600

    step 4 test git action

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
-import lowLvlApp from '../scr/cli.js'
+/* eslint-disable import/no-named-as-default */
+// eslint-disable-next-line import/no-named-as-default-member
+import lowLvlApp from '../scr/cli.js';
+
 console.log('Welcome to the Brain Games!');
 lowLvlApp();
--- scr/cli.js
+++ scr/cli.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
+
 const lowLvlApp = () => {
-const name = readlineSync.question('Your answer: ');
-console.log('Hello, ' + name + '!');
+  const name = readlineSync.question('Your answer: ');
+  console.log(`Hello, ${name}!`);
 };
 export default lowLvlApp;
