e9ded083be5b996a99c855c630241302fed8a768
Ivan Sharapov <oldrum75@gmail.com>
2022-03-27 22:39:39 +0600

    brain-gcd + fix

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -7,16 +7,13 @@ const braingcd = () => {
   console.log('brain-gcd');
   startSay();
   console.log('Find the greatest common divisor of given numbers.');
-
   for (let i = 0; i < 3; i += 1) {
     const num1 = getRandomInt(1, 20);
     const num2 = getRandomInt(1, 20);
     let Maxnum = 0;
     let zxc = 0;
     if (num1 < num2) { Maxnum = num1; } else { Maxnum = num2; } // находим наибольшее
-
     console.log(`Question: ${num1} ${num2}`);
-
     for (let v = Maxnum; v > 0; v -= 1) {
       if (num1 % v === 0 && num2 % v === 0) {
         // eslint-disable-next-line no-unused-vars
@@ -24,9 +21,7 @@ const braingcd = () => {
         break;
       }
     }
-
     const resultUser = Number(readlineSync.question('Your answer: '));
-
     if (resultUser === zxc) {
       console.log('Correct!');
     } else {
