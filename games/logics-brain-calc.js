1ce179a0e70206c32020d0877d0ee045f90fae43
Ivan Sharapov <oldrum75@gmail.com>
2022-03-31 19:48:39 +0600

    test true index

--- games/logics-brain-calc.js
+++ games/logics-brain-calc.js
@@ -1,43 +1,35 @@
-import readlineSync from 'readline-sync';
 import getRandomInt from '../scr/modRandom.js';
-import { startSay, name } from '../scr/cli.js';
+import startgreetings from '../scr/index.js';
+
+console.log('brain-calc');
+startSay();
+console.log('What is the result of the expression?');
 
 const braincalc = () => {
-  console.log('brain-calc');
-  startSay();
-  console.log('What is the result of the expression?');
-  for (let i = 0; i < 3; i += 1) {
-    const num1 = getRandomInt(1, 10);
-    const num2 = getRandomInt(1, 10);
-    const arr = ['+', '-', '*'];
-    const counter = arr[getRandomInt(0, 2)];
+  const num1 = getRandomInt(1, 10);
+  const num2 = getRandomInt(1, 10);
+  const arr = ['+', '-', '*'];
+  const counter = arr[getRandomInt(0, 2)];
 
-    let inputSystem = 0;
+  let inputSystem = 0;
 
-    switch (counter) {
-      case '+':
-        inputSystem = num1 + num2;
-        break;
-      case '-':
-        inputSystem = num1 - num2;
-        break;
-      case '*':
-        inputSystem = num1 * num2;
-        break;
-      default:
-    }
-    console.log(`Question: ${num1} ${counter} ${num2}`);
-    const inputUser = Number(readlineSync.question('Your answer: '));
-
-    if (inputSystem === inputUser) {
-      console.log('Correct!');
-    } else {
-      console.log(`${inputUser} is wrong answer ;(. Correct answer was ${inputSystem}.`);
-      console.log(`Let's try again, ${name}`);
-      return;
-    }
+  switch (counter) {
+    case '+':
+      inputSystem = num1 + num2;
+      break;
+    case '-':
+      inputSystem = num1 - num2;
+      break;
+    case '*':
+      inputSystem = num1 * num2;
+      break;
+    default:
   }
-  console.log(`Congratulations, ${name}!`);
+  String(inputSystem);
+  const unit = (`${num1} ${counter} ${num2}`);
+  return (unit, inputSystem);
 };
 
-export default braincalc;
+const startcalc = () => {
+  braincalc();
+}
